6a318db98d38c79985f9e0f5ebe804e25523ee65
Chandra Mannava <mac@MacBookPro-487.lan>
2025-11-06 16:50:37 -0600

    complete code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Zomato Chatbot",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "chainlit",
+    ],
+)
